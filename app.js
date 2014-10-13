e611691894bcbcb7694a0433ea889b7ae42b1bc5
YourMatt <github@yourmatt.com>
2014-10-13 08:03:08 -0600

    Set to use environment port.

--- app.js
+++ app.js
@@ -3,17 +3,18 @@ var express = require ("express"),
     skillmapsvg = require ("./errorreporter.js");
 
 var app = express ();
+app.set ("port", (process.env.PORT || 80));
 app.use (express.static (__dirname + "/public"));
 
 // handle all other requests
 app.set ("views", __dirname + "/views/layout");
 app.engine ("ejs", require ("ejs").renderFile);
-app.get ("/*", function (req, res) {
+app.get ("/", function (req, res) {
 
     res.render ("template.ejs");
 
 });
 
 // start server
-app.listen (80);
+app.listen (app.get ("port"));
 console.log ("Server started.");
