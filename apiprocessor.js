f45776baeca6ae88c9333c33ff8b6fcec9ec9102
YourMatt <github@yourmatt.com>
2014-10-14 08:07:53 -0600

    Changed to non-standard basic authentication because Heroku doesn't pass through authentication HTTP param

--- apiprocessor.js
+++ apiprocessor.js
@@ -3,6 +3,19 @@ exports.processor = {
 
     authenticate: function (req) {
 
+        var checkUser = req.headers.username;
+        var checkPassword = req.headers.password;
+
+        // TODO: Check database for user
+        if (checkUser != "user" || checkPassword != "password") return false;
+
+        return true;
+
+    },
+
+    // check HTTP basic authentication - removed because Heroku doesn't send these HTTP headers to the app
+    authenticateBasic: function (req) {
+
         if (! req.headers.authorization) return false;
         var authParams = new Buffer(req.headers.authorization.substring(6), 'base64').toString().split(':');
         if (! authParams) return false;
